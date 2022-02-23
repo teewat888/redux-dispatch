10599b5c660fffe30387c2ae715be0d70efaf477
TEERAWAT PRAROM <teewat@yahoo.com>
2022-02-23 23:21:42 +1100

    completed assignment

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,24 @@
-function changeState(state, action){
+let state = { count: 0 };
+
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
 
-changeState(state, action)
+function render() {
+  document.body.textContent = state.count;
+}
+
+dispatch({ type: "INCREASE_COUNT" });
+dispatch({ type: "INCREASE_COUNT" });
+dispatch({ type: "INCREASE_COUNT" });
+dispatch({ type: "INCREASE_COUNT" });
